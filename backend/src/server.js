7500700626bc003a58dd73708668ecae7d80436d
saikartik2625 <saikartik2005@gmail.com>
2026-02-15 23:46:28 +0530

    prepared for development

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import cors from "cors"
-
+import path from "path"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
@@ -12,21 +12,34 @@ import rateLimiter from "./middleware/rateLimiter.js";
 
 const app = express();
 const PORT = process.env.PORT || 5001
-
+const __dirname = path.resolve()
 
 
 
 //middleware
-app.use(cors({
-   origin:"http://localhost:5173",
-}));
+if(process.env.NODE_ENV !== "production"){
+app.use(
+   cors({
+    origin:"http://localhost:5173",
+})
+);
+}
+
+
+
 app.use(express.json()); //this middle war will parse json body
 app.use(rateLimiter)
 
 
 app.use("/api/notes",notesRoutes);
  
+if(process.env.NODE_ENV === "production"){
+   app.use(express.static(path.join(__dirname,"../frontend/dist")))
 
+app.get("*",(req,res) => {
+   res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+});
+}
 
 connectDB().then(() => { 
  app.listen(PORT, () => {
